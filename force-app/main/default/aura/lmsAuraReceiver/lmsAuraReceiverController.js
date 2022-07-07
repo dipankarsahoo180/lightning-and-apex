9b0a235f18f3a9a58b036e73c3518673691ddd69
Dipankar Sahoo <disahoo@hcg.com>
2022-07-07 21:50:02 +0530

    Aura publish and Subscribe using LMS

--- force-app/main/default/aura/lmsAuraReceiver/lmsAuraReceiverController.js
+++ force-app/main/default/aura/lmsAuraReceiver/lmsAuraReceiverController.js
@@ -2,7 +2,7 @@
     // This function is used to listen and subscribe the publish value
     handleReceivedMessage : function(component, message, helper) { 
         // dipsFieldOne is the field name set in the message channel
-        let receivedMessage = message.getParam("dipsFieldOne").value?message.getParam("dipsFieldOne").value: message.getParam("dipsFieldOne");
+        let receivedMessage = message.getParam("dipsFieldOne").value;
         component.set("v.messageReceived",receivedMessage)
 
     }
