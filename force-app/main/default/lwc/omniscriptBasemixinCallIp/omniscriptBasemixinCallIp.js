01a440ca015eeac5c3c7039222c0fc833261e167
dipankarsahoo180 <dipankarsahoo180@gmail.com>
2023-08-04 14:21:46 +0530

    Call Integration Procedure from lwc

--- force-app/main/default/lwc/omniscriptBasemixinCallIp/omniscriptBasemixinCallIp.js
+++ force-app/main/default/lwc/omniscriptBasemixinCallIp/omniscriptBasemixinCallIp.js
@@ -5,16 +5,19 @@ import {OmniscriptBaseMixin} from 'omnistudio/omniscriptBaseMixin';
 export default class OmniscriptBasemixinCallIp extends OmniscriptBaseMixin(LightningElement) {
     
     @track selectedContacts=[];
-    //update - event name
-    //handleUpdateContacts - handler to call
+    @track deletedContact;
+
+    //event name: handlerName
     pubsubPayload = {
         update: this.handleUpdateContacts.bind(this),
         checkboxChanged: this.handleUpdateSelectedContacts.bind(this),
+        deleteContact: this.handleRemoveDeletedContacts.bind(this),
     };
 
     connectedCallback() {
         pubsub.register('flexcard_datatable_row', this.pubsubPayload); //flexcard_datatable_row = channel name
-        pubsub.register('flexcard_selected_row', this.pubsubPayload); //flexcard_datatable_row = channel name
+        pubsub.register('flexcard_selected_row', this.pubsubPayload);
+        pubsub.register('flexcard_action_delete_contact', this.pubsubPayload); 
         this.selectedContacts = this.omniJsonData?.selectedContacts || [];
     }
 
@@ -28,13 +31,22 @@ export default class OmniscriptBasemixinCallIp extends OmniscriptBaseMixin(Light
             this.selectedContacts = this.selectedContacts.filter(el=>el.ContactId != result.ContactId);
         }
         this.selectedContacts.sort((a, b)=>{return parseInt(a.originalIndex) - parseInt(b.originalIndex)});
-        this.omniApplyCallResp({selectedContacts:this.selectedContacts});
+        this.omniApplyCallResp({Step1:{selectedContacts:this.selectedContacts}});
+    }
+
+    handleRemoveDeletedContacts(evt){
+        console.warn('deleted row',JSON.parse(JSON.stringify(evt?.inputPayload)));
+        const result = evt?.inputPayload;
+        this.deletedContact = result;
+        this.selectedContacts = this.selectedContacts.filter(el=>el.ContactId != result.ContactId);
+        this.omniApplyCallResp({Step1:{selectedContacts:this.selectedContacts}});
     }
 
     handleUpdateContacts(evt){
-        console.log('Full Payload is ',evt?.FullPayload)
+        console.log('Full Payload is ',evt?.FullPayload, typeof evt.FullPayload, Array.isArray(evt.FullPayload));
+        const Payload = evt.FullPayload?.filter(el=>el.ContactId != this.deletedContact?.ContactId);
         const input = {
-            ContactList: evt.FullPayload
+            ContactList: Payload
         };
 
         //IP Call Starts
@@ -53,8 +65,9 @@ export default class OmniscriptBasemixinCallIp extends OmniscriptBaseMixin(Light
 
         util.getDataHandler(JSON.stringify(request_getIPRemote)).then((result) => {
             result = JSON.parse(result);  
-            let resp = result.IPResult?.IP_RatingArea || '';
-            console.log('IP result is ',result.IPResult);
+            console.log('Response ',result);
+            let resp = result.IPResult || '';
+            console.log('IP result is ',resp);
         }).catch((error)=>{
             console.log ('IP error is : ',error);
         })
@@ -63,5 +76,6 @@ export default class OmniscriptBasemixinCallIp extends OmniscriptBaseMixin(Light
     disconnectedCallback() {
         pubsub.unregister('flexcard_datatable_row', this.pubsubPayload);
         pubsub.unregister('flexcard_selected_row', this.pubsubPayload);
+        pubsub.unregister('flexcard_action_delete_contact', this.pubsubPayload);
     }
 }
