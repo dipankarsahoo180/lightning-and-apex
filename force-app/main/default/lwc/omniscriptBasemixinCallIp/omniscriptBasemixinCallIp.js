7f7717617ffa5145e3b1f40c4083282cb3ebe700
Dipankar Sahoo <dipankarsahoo180@gmail.com>
2025-10-19 19:18:23 +0530

    update omniscript with selected rows from flexcard

--- force-app/main/default/lwc/omniscriptBasemixinCallIp/omniscriptBasemixinCallIp.js
+++ force-app/main/default/lwc/omniscriptBasemixinCallIp/omniscriptBasemixinCallIp.js
@@ -7,44 +7,74 @@ export default class OmniscriptBasemixinCallIp extends OmniscriptBaseMixin(Light
     @track selectedContacts=[];
     @track deletedContact;
 
-    //event name: handlerName
+    //event handler object: contains list of all the pubsub events
     pubsubPayload = {
-        update: this.handleUpdateContacts.bind(this),
-        checkboxChanged: this.handleUpdateSelectedContacts.bind(this),
-        deleteContact: this.handleRemoveDeletedContacts.bind(this),
+        update: this.handleUpdateContacts.bind(this), //update is the event name set in the flexcard along with channel name flexcard_datatable_row
+        checkboxChanged: this.handleUpdateSelectedContacts.bind(this), //checkboxChanged is the event name set in the flexcard along with channel name flexcard_selected_row
+        deleteContact: this.handleRemoveDeletedContacts.bind(this), //can be used as per need. deleteContact is the event name set in the flexcard along with channel name flexcard_action_delete_contact.
+        data: this.omniNextStep.bind(this), //if event fired go to next step
     };
 
+    //register the pubsub channels
     connectedCallback() {
-        pubsub.register('flexcard_datatable_row', this.pubsubPayload); //flexcard_datatable_row = channel name
+        pubsub.register('flexcard_datatable_row', this.pubsubPayload); //flexcard_datatable_row is pubsub channel name, this.pubsubPayload contains the event
         pubsub.register('flexcard_selected_row', this.pubsubPayload);
-        pubsub.register('flexcard_action_delete_contact', this.pubsubPayload); 
-        this.selectedContacts = this.omniJsonData?.selectedContacts || [];
+        pubsub.register('flexcard_action_delete_contact', this.pubsubPayload);
+        pubsub.register("omniscript_action", this.pubsubPayload);
+        let jsonData = JSON.parse(JSON.stringify(this.omniJsonData));
+        this.selectedContacts = jsonData.Step1?.selectedContacts || [];
+        this.Contacts = jsonData.Contacts || [];
+        if(this.selectedContacts?.length >0){
+            this.Contacts?.forEach(el=>el.selectrow = false);
+            this.omniApplyCallResp({Contacts:this.Contacts});
+        }
     }
 
+    omniNextStep(){
+        super.omniNextStep();
+    }
+
+    //dynamically add/update the omniscript json with the selected rows contacts
     handleUpdateSelectedContacts(evt){
         console.warn('selected row',JSON.parse(JSON.stringify(evt?.selectedRow)));
         const result = evt?.selectedRow;
-        if(result.selectrow){
+        if(result.selectrow){ //for individual rows checkbox selected
             this.selectedContacts.push(result);
+        }else if(evt?.selectedRow === "all"){ //if top check box is selected to select all rows
+            this.selectedContacts = evt.records;
+            this.selectedContacts = this.selectedContacts?.map(el=> ({...el, selectrow : true}));
+        }else if(evt?.selectedRow === "none"){ //if top check box is selected to de-select all rows
+            this.selectedContacts = [];
         }
         else{
-            this.selectedContacts = this.selectedContacts.filter(el=>el.ContactId != result.ContactId);
+            this.selectedContacts = this.selectedContacts.filter(el=>el.Id != result.Id); //for individual rows checkbox de-selected
+            //if (this.selectedContacts?.length === 0) pubsub.fire('pubsub_refresh_card','refresh_state',{});
         }
-        this.selectedContacts.sort((a, b)=>{return parseInt(a.originalIndex) - parseInt(b.originalIndex)});
-        this.omniApplyCallResp({Step1:{selectedContacts:this.selectedContacts}});
+        this.selectedContacts = this.selectedContacts?.filter((obj, index, self) => index === self?.findIndex((o) => o?.Id === obj?.Id)); //to filter out duplicates
+        this.Contacts = this.Contacts?.map(originalContact=>{
+            if(this.selectedContacts?.find(el=>el.Id==originalContact.Id)?.selectrow == true) originalContact.selectrow = true;
+            else originalContact.selectrow = false;
+            return originalContact;
+        });
+        this.omniApplyCallResp({Contacts:this.Contacts});
+        //this.selectedContacts.sort((a, b)=>{return parseInt(a.originalIndex) - parseInt(b.originalIndex)}); //sort the results based on index
+        console.log('this.selectedContacts ',JSON.parse(JSON.stringify(this.selectedContacts)));
+        this.omniUpdateDataJson(this.selectedContacts, true); //update omniscript with the selected rows
     }
 
+    //removes the deleted row from data table from json of omniscript.
     handleRemoveDeletedContacts(evt){
         console.warn('deleted row',JSON.parse(JSON.stringify(evt?.inputPayload)));
         const result = evt?.inputPayload;
         this.deletedContact = result;
-        this.selectedContacts = this.selectedContacts.filter(el=>el.ContactId != result.ContactId);
-        this.omniApplyCallResp({Step1:{selectedContacts:this.selectedContacts}});
+        this.selectedContacts = this.selectedContacts.filter(el=>el.Id != result.Id); //remove deleted contact from selected rows list
+        this.omniUpdateDataJson(this.selectedContacts, true); //update omniscript after removing deleted row
     }
 
+    //updates the data updated through data table using an integration procedure.
     handleUpdateContacts(evt){
         console.log('Full Payload is ',evt?.FullPayload, typeof evt.FullPayload, Array.isArray(evt.FullPayload));
-        const Payload = evt.FullPayload?.filter(el=>el.ContactId != this.deletedContact?.ContactId);
+        const Payload = evt.FullPayload?.filter(el=>el.Id != this.deletedContact?.Id);
         const input = {
             ContactList: Payload
         };
@@ -73,6 +103,7 @@ export default class OmniscriptBasemixinCallIp extends OmniscriptBaseMixin(Light
         })
     }
 
+    //un-register the pubsub channels
     disconnectedCallback() {
         pubsub.unregister('flexcard_datatable_row', this.pubsubPayload);
         pubsub.unregister('flexcard_selected_row', this.pubsubPayload);
